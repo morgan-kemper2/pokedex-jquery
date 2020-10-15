5acf8b714692233631f3373a64a641f669332aa3
morgan-kemper2 <71855461+morgan-kemper2@users.noreply.github.com>
2020-10-15 12:23:42 -0500

    added 5 pokemon with height and type to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello World!');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+pokemonList = [
+  {
+    name: Bulbasaur,
+    height: 7,
+    type: {'grass', 'poison'}
+  },
+  {
+    name: Wartortle,
+    height: 1,
+    type: 'water'
+  },
+  {
+    name: Beedrill,
+    height: 1,
+    type: {'bug', 'poison'}
+  }
+  {
+    name: Fearow,
+    height: 1.2,
+    type: {'flying', 'normal'}
+  }
+  {
+    name: Nidoran,
+    height: 0.4,
+    type: 'poison'
+  }
+];
