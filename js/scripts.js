c3065bfa646d323521297a289f87e458d71f4018
morgan-kemper2 <71855461+morgan-kemper2@users.noreply.github.com>
2020-10-15 18:36:07 -0500

    added loop to display pokemon names, height and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
-pokemonList = [
+let pokemonList = [
   {
-    name: Bulbasaur,
-    height: 7,
-    type: {'grass', 'poison'}
+    name: "Bulbasaur",
+    height: 7.0,
+    type: ["Grass", "Poison"]
   },
   {
-    name: Wartortle,
-    height: 1,
-    type: 'water'
+    name: "Wartortle",
+    height: 1.0,
+    type: ["Water"]
   },
   {
-    name: Beedrill,
-    height: 1,
-    type: {'bug', 'poison'}
-  }
+    name: "Beedrill",
+    height: 1.0,
+    type: ["Bug", "Poison"]
+  },
   {
-    name: Fearow,
+    name: "Fearow",
     height: 1.2,
-    type: {'flying', 'normal'}
-  }
+    type: ["Flying", "Normal"]
+  },
   {
-    name: Nidoran,
+    name: "Nidoran",
     height: 0.4,
-    type: 'poison'
+    type: ["Poison"]
   }
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(' Name: ' + pokemonList[i].name + ' /Height: ' + pokemonList[i].height + ' /Type: ' + pokemonList[i].type);
+  document.write("<br>");
+  if (pokemonList[i].height >= 4) {
+    document.write("Wow! That is Big!");
+    document.write("<br>");
+  } else {
+    document.write("");
+  }
+};
